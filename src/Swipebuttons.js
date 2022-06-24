7b7c5d4bfc405c8d59454c454bfbf0a8237d2222
Abhish1sb <abhish1albodi@gmail.com>
2022-06-24 17:45:58 +0530

    adding bottom bar

--- src/Swipebuttons.js
+++ src/Swipebuttons.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import "./Swipebuttons.css";
+import ReplayIcon from '@mui/icons-material/Replay';
+import CloseIcon from '@mui/icons-material/Close';
+import StarRateIcon from '@mui/icons-material/StarRate';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FlashOnIcon from '@mui/icons-material/FlashOn';
+import IconButton from '@mui/material/IconButton';
 
 function Swipebuttons() {
   return (
-    <div className="Swipebuttons">
-        <h3>Swipebuttons</h3>
+    <div className="swipebuttons">
+        {/* <h3>Swipebuttons</h3> */}
+        <IconButton className="swipebuttons_replay">
+            <ReplayIcon fontSize='large' />
+        </IconButton>
+        <IconButton className="swipebuttons_close">
+            <CloseIcon fontSize='large' />
+        </IconButton>
+        <IconButton className="swipebuttons_star">
+            <StarRateIcon fontSize='large' />
+        </IconButton>
+        <IconButton className="swipebuttons_favourite">
+            <FavoriteIcon fontSize='large' />
+        </IconButton>
+        <IconButton className="swipebuttons_flashon">
+            <FlashOnIcon fontSize='large' />
+        </IconButton>
+
     </div>
   )
 }
